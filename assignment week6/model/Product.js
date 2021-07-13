309e8818ad29a6fe9d99410c9dc12b469e38c8bd
fejiro <85490641+fejismoney@users.noreply.github.com>
2021-07-13 10:55:09 +0100

    e-commerce

---
+++ assignment week6/model/Product.js
@@ -0,0 +1,85 @@
+let products = [
+    {
+        id: 1,
+        name: "Hyundai i10",
+        description: "Small, economical, practical and super easy to drive.",
+        image: "https://www.carstore.com/-/media/carstore/makes/hyundai/i10/hyundai-i10-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=783C00C261E828EA47B2DDCC4DF01DBC",
+        price: 2200000
+    },
+
+    {
+        id: 2,
+        name: "Honda Civic",
+        description: "A popular hatchback with futuristic and striking style.",
+        image: "https://www.carstore.com/-/media/carstore/makes/honda/civic/honda-civic-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=1AC266853C61DDB67E412DAFCA96CAFE",
+        price: 1500000
+    },
+
+    {
+        id: 3,
+        name: "Ford Mondeo",
+        description: "A textbook family car with solid build quality and multi award-winning engines.",
+        image: "https://www.carstore.com/-/media/carstore/makes/ford/mondeo/ford-mondeo-front-thumbnail-480x270.ashx?mh=519&la=en&h=290&w=500&mw=519&hash=879EC4B92C6F4B7361D32B7F76C6337F",
+        price: 1800000
+    },
+
+    {
+        id: 4,
+        name: "Fiat 500",
+        description: "Curvy, cute and compact, a fantastic city car.",
+        image: "https://www.carstore.com/-/media/carstore/makes/fiat/500/fiat-500-red-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=BF55D2EE4EBEDA8FA30C11D071B7A93A",
+        price: 2000000
+    },
+
+    {
+        id: 5,
+        name: "Range Rover",
+        description: "A premium SUV offering the best comfort on the road.",
+        image: "https://www.carstore.com/-/media/carstore/makes/land-rover/range-rover/land-rover-blue-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=A592FC91A28123826389EF9E4BD6B7D6",
+        price: 5000000
+    },
+
+    {
+        id: 6,
+        name: "Kia Sportage",
+        description: "A popular SUV with lots of equipment as standard.",
+        image: "https://www.carstore.com/-/media/carstore/makes/kia/sportage/kia-sportage-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=F1FFE82ADAEEEEE9DB4A43118D104109",
+        price: 3500000
+    },
+
+    {
+        id: 7,
+        name: "Nissan Micra",
+        description: "A superbly reliable, easy to drive and cheap to run supermini.",
+        image: "https://www.carstore.com/-/media/carstore/makes/nissan/micra/high-res-images/nissian-micra-thumbnail-480x270px.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=FF11119152082C846A49FD03D03696A2",
+        price: 1000000
+    },
+
+    {
+        id: 8,
+        name: "Audi A1",
+        description: "A claasy supermini with a fresh sporty design.",
+        image: "https://www.carstore.com/-/media/carstore/makes/audi/a1/audi-a1-grey-front-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=A889989AA131BBA5E4ED0227B0F887F1",
+        price: 1800000
+    },
+
+    {
+        id: 9,
+        name: "BMW X1",
+        description: "Meet the smallest of the SUV range. Comfortable, good looking yet engaging to drive.",
+        image: "https://www.carstore.com/-/media/carstore/makes/bmw/x1/bmw-x1-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=D30BBC7BCE77D0C4E5D0B13537038A16",
+        price: 900000
+    },
+
+    {
+        id: 10,
+        name: "Toyota Aygo",
+        description: "A great city car that is good-looking, and comfortable to drive.",
+        image: "https://www.carstore.com/-/media/carstore/makes/toyota/aygo/toyota-aygo-front-thumbnail-480x270.ashx?mh=519&la=en&h=270&w=480&mw=519&hash=CFA7FE7F9BBBE18561257BD9108E3589",
+        price: 6000000
+    }
+
+
+];
+
+module.exports = products;
